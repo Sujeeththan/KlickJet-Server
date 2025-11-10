780c16a7a8869d7cdcda7d7d36c4851c332c0006
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-10 20:27:16 +0530

    All user routes require admin authentication create

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,4 @@
 import express from "express";
-
 import {
   getAllUsers,
   getUserById,
@@ -7,13 +6,16 @@ import {
   updateUser,
   deleteUser,
 } from "../controllers/userController.js";
+import { verifyToken } from "../middleware/auth.js";
+import { verifyRole } from "../middleware/roleMiddleware.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/", getAllUsers);
-userRouter.get("/:id", getUserById);
-userRouter.post("/", createUser);
-userRouter.put("/:id", updateUser);
-userRouter.delete("/:id", deleteUser);
+// All user routes require admin authentication
+userRouter.get("/", verifyToken, verifyRole("admin"), getAllUsers);
+userRouter.get("/:id", verifyToken, verifyRole("admin"), getUserById);
+userRouter.post("/", verifyToken, verifyRole("admin"), createUser);
+userRouter.put("/:id", verifyToken, verifyRole("admin"), updateUser);
+userRouter.delete("/:id", verifyToken, verifyRole("admin"), deleteUser);
 
 export default userRouter;
