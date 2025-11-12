b1c963d7cb71b5ce1816897c5c59a9a280ead289
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-12 07:31:29 +0530

    db.js  mongo connection

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+  }
+};
+
+export default connectDB;
+
