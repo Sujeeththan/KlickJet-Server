57f1d84cb75d65be47239c9de505f3eecb1f78d1
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-12 07:44:36 +0530

    reviewRoute impliment

---
+++ src/routes/reviewRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+  getAllReviews,
+  getReviewById,
+  createReview,
+  updateReview,
+  deleteReview,
+} from "../controllers/reviewController.js";
+import { verifyToken, optionalAuth } from "../middleware/auth.js";
+import { verifyRole } from "../middleware/roleMiddleware.js";
+
+const reviewRouter = express.Router();
+
+// Public routes - anyone can view reviews
+reviewRouter.get("/", optionalAuth, getAllReviews);
+reviewRouter.get("/:id", optionalAuth, getReviewById);
+
+// Protected routes - customers can create/update/delete their reviews
+reviewRouter.post("/", verifyToken, verifyRole("customer"), createReview);
+reviewRouter.put("/:id", verifyToken, verifyRole(["admin", "customer"]), updateReview);
+reviewRouter.delete("/:id", verifyToken, verifyRole(["admin", "customer"]), deleteReview);
+
+export default reviewRouter;
+
